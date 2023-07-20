b4888756f5d6a438dda88dbf8586cf73fdacbc65
meet_hansalia <meethansalia99@gmail.com>
2023-07-20 17:11:20 +0530

    sorting logic

--- quote/src/component/quotes/QuoteList.js
+++ quote/src/component/quotes/QuoteList.js
@@ -1,12 +1,44 @@
 import {Fragment} from 'react'
 import './QuoteList.css'
 import QuoteItem from './QuoteItem.js';
+import { useHistory, useLocation } from 'react-router-dom';
+
+
+const sortQuotes = (quotes, ascending) => {
+    return quotes.sort((quoteA, quoteB) => {
+      if (ascending) {
+        return quoteA.id > quoteB.id ? 1 : -1;
+      } else {
+        return quoteA.id < quoteB.id ? 1 : -1;
+      }
+    });
+  };
+
+
 
 const QuoteList = (props) =>{
+    const history = useHistory();
+    const location = useLocation()
+    
+    const queryParams = new URLSearchParams(location.search)
+
+    const isSortingAscending = queryParams.get('sort') === 'asc';
+
+    const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+
+    const changeSortingHandler =()=>{
+        history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+    }
+
     return(
         <Fragment>
+            <div className="sorting">
+                <button onClick={changeSortingHandler}>
+                    Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+                </button>
+            </div>
             <ul className="list"> 
-                {props.quotes.map((quote)=>(
+                {sortedQuotes.map((quote)=>(
                     <QuoteItem
                         key={quote.id}
                         id={quote.id}
@@ -16,6 +48,7 @@ const QuoteList = (props) =>{
 
                 ))}
             </ul>
+      
         </Fragment>
     )
 }
